b34ebcf80a4a4afce466e848ce17e03ef5b30fc0
duoduo <2326064185@qq.com>
2022-12-31 11:07:40 +0000

    优化 build 和 Compile 脚本

--- GCC-build.sh
+++ GCC-build.sh
@@ -1,7 +1,8 @@
-shopt -s expand_aliases
-alias trace_on='set -x'
-alias trace_off='{ set +x; } 2>/dev/null'
-set -e
+export SHELLOPTS                            # 继承环境变量
+shopt -s expand_aliases                     # 启用 alias
+alias trace_on='set -x'                     # 启用 打印执行的命令
+alias trace_off='{ set +x; } 2>/dev/null'   # 关闭 打印执行的命令
+set -e                                      # 当遇到错误就停止
 
 
 trace_on
