9f8cca6cc13372c2a7052352484d32e9d68bad55
duoduo <2326064185@qq.com>
2023-01-17 11:05:42 +0000

    bug

--- src/crypto.c
+++ src/crypto.c
@@ -3,7 +3,22 @@
 
 #define ERROR1 "请输入算法和消息\n"
 
-long argv_len(char const str[]){
+struct node{
+  int (*p)(unsigned int argc, char *argv[]);
+  char cmd[];
+};
+
+
+int SHA1(unsigned int argc, char *argv[]){
+    
+    return 0;
+}
+
+struct node alg[1] = {
+    {&SHA1,"SHA-1"},
+};
+
+long str_len(char str[]){
     unsigned long i = 0;
     char ASCII = 0xFF;
     while (ASCII != 0)
@@ -21,7 +36,7 @@ int main(unsigned int argc, char *argv[]){
     }
     if (argc == 3){
         if (argv[1] == "S"){
-            unsigned long len = argv_len(argv[2]);
+            unsigned long len = str_len(argv[2]);
             unsigned int *hash = SHA1(argv[2],len);
             printf("%x%x%x%x%x\n",hash[0],hash[1],hash[2],hash[3],hash[4]);
         }
